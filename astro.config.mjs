95fdfd4dbec27b24446ac82324199b0002701b4e
tyryotoy <tyryotoy@gmail.com>
2023-01-31 04:18:45 +0900

    chore: add various integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [react(), tailwind(), partytown()]
 });
